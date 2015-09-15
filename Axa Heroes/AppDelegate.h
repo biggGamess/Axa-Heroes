644b013895c7cc9c468b2f385cb3e8d9f4a03d7a
Marc Gallardo <marcgallardo@Mac-mini-de-Marc.local>
2015-09-15 11:50:48 +0200

    Initial Commit

---
+++ Axa Heroes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Axa Heroes
+//
+//  Created by Marc Gallardo on 15/9/15.
+//  Copyright (c) 2015 Gallardos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Axa Heroes/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Axa Heroes
+//
+//  Created by Marc Gallardo on 15/9/15.
+//  Copyright (c) 2015 Gallardos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
